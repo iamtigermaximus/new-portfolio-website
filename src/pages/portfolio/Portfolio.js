d71cedd80103e21934d643070f91b9b465cfc46e
Siegfred Gamboa <siegy.gamboa@gmail.com>
2022-11-28 18:47:21 +0200

    refactor portfolio page, added loader

--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -7,16 +7,24 @@ import {
   HeadingContainer,
   Heading,
   ProjectContainer,
+  LoadingContainer,
+  Loader,
 } from './Portfolio.styles'
 import ProjectCard from '../../components/project-card/ProjectCard'
 import axios from 'axios'
 
 const Portfolio = () => {
   const [projects, setProjects] = useState([])
+  const [loading, setLoading] = useState(false)
+
   useEffect(() => {
+    setLoading(true)
     axios
       .get('https://portfolio-backend-rosy.vercel.app/')
       .then((response) => setProjects(response.data.items))
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
   }, [])
 
   return (
@@ -27,23 +35,29 @@ const Portfolio = () => {
           <Heading>Projects</Heading>
         </HeadingContainer>
       </Fade>
-      <Fade direction='left'>
-        <ProjectContainer>
-          {projects
-            .slice()
-            .reverse()
-            .map((project) => (
-              <ProjectCard
-                key={project.id}
-                id={project.id}
-                image={project.image}
-                title={project.title}
-                tech={project.tech}
-                demo={project.demoLink}
-                github={project.github}
-              />
-            ))}
-        </ProjectContainer>
+      <Fade direction='left' delay='10000s'>
+        {loading ? (
+          <LoadingContainer>
+            <Loader>LOADING...</Loader>
+          </LoadingContainer>
+        ) : (
+          <ProjectContainer>
+            {projects
+              .slice()
+              .reverse()
+              .map((project) => (
+                <ProjectCard
+                  key={project.id}
+                  id={project.id}
+                  image={project.image}
+                  title={project.title}
+                  tech={project.tech}
+                  demo={project.demoLink}
+                  github={project.github}
+                />
+              ))}
+          </ProjectContainer>
+        )}
       </Fade>
     </Container>
   )
--- src/pages/portfolio/Portfolio.styles.js
+++ src/pages/portfolio/Portfolio.styles.js
@@ -53,3 +53,13 @@ export const ProjectContainer = styled.div`
     //padding: 30px;
   }
 `
+
+export const LoadingContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50vh;
+`
+export const Loader = styled.h1`
+  font-size: 30px;
+`
